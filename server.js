e8d23d5a56ba5d6ad8e2d9db14bdd60290a46cdd
Ubmsr <bitzytusk@gmail.com>
2025-03-18 16:54:31 -0400

    Add new routes to server

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 // Import Express
-const express = require('express');
-
-// Create an Express app
+const express = require("express");
 const app = express();
-
-// Define a port for the server
-const PORT = 3000;
+const PORT = 3000; // Set port as such
 
 // Define a basic route ("/")
-app.get('/', (req, res) => {
-    res.send('Hello, Express is working!');
+app.get("/", (req, res) => {
+  res.send("Hello, Express is working!");
+});
+
+app.get("/check", (req, res) => {
+  res.send("This is the /check endpoint.");
+});
+
+app.get("/user/:name", (req, res) => {
+  const userName = req.params.name;
+  res.send("Hello, ${userName}!");
 });
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
-
